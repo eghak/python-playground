a8f7aafdd23455acc984bc2d0b7ad450eaaceaf4
Egha Kusuma <eghakusuma1@gmail.com>
2023-12-28 23:08:49 -0700

    update

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "John Doe"
+
+print(len(name)) #length
+print(name.find("J")) #find character
+print(name.capitalize()) #capitalize the first letter of the string
+print(name.upper()) #uppercase
+print(name.lower()) #lowercase
+print(name.isdigit()) #return boolean if it's digit
+print(name.isalpha()) #return boolean if it's alphabet
+print(name.count("o")) #count character
+print(name.replace("o", "a")) #replace character
+print(name*4) #print the string a the number time
